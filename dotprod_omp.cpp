d07e62dcf1f5e7773ff166e622023a2a73e9e2de
Torbjörn Klatt <torbjoern.k@googlemail.com>
2012-03-22 20:03:19 +0100

    exluding KDevelop4 project files

--- dotprod_omp.cpp
+++ dotprod_omp.cpp
@@ -22,16 +22,18 @@ int main( int argn, char *arg[] )
   int i;
   
   clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &start1 );
+  
   for ( i = 0; i < N; i++ ) {
     a[i] = i * 0.5;
     b[i] = i * 2.0;
   }
+  
   clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &start1 );
   
   sum = 0;
   
   clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &start2 );
-  #pragma omp parallel for \
+#pragma omp parallel for \
     default(shared) \
     shared(a, b) \
     reduction(+:sum) \
@@ -39,11 +41,10 @@ int main( int argn, char *arg[] )
   for ( i = 0; i < N; i++ ) {
     sum = sum + a[i] * b[i];
   }
+  
   clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &start2 );
   
   printf( "sum = %f\n", sum );
-  printf( "Initialization\n%u sec % 10u nsec\n",
-          start1.tv_sec, start1.tv_nsec );
-  printf( "Addition\n%u sec % 10u nsec\n",
-          start2.tv_sec, start2.tv_nsec );
+  printf( "Initialization\n%u sec % 10u nsec\n", start1.tv_sec, start1.tv_nsec );
+  printf( "Addition\n%u sec % 10u nsec\n", start2.tv_sec, start2.tv_nsec );
 }
