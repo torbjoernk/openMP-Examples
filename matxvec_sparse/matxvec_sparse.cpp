e3e9dc563ac6848fcc2b9de82a9e1d917bef7778
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2012-03-26 01:39:24 +0200

    fixed: matxvec_sparse

--- matxvec_sparse/matxvec_sparse.cpp
+++ matxvec_sparse/matxvec_sparse.cpp
@@ -5,7 +5,7 @@
 
 #define N_COL 5
 #define N_ROW 4
-#define NNZ 6
+#define NNZ 7
 
 void printSparseMat ( double val[], int colInd[], int rowPt[] );
 void printVec ( double val[], int size );
@@ -17,8 +17,8 @@ void mxv ( double * __restrict__ aval, int * __restrict__ acolind, int * __restr
  */
 int main( int argn, char *arg[] )
 {
-  srand( 123 );
-  omp_set_num_threads(4);
+  srand( 1234567 );
+  omp_set_num_threads(2);
   
   std::cout << "Number columns:      " << N_COL << std::endl;
   std::cout << "Number rows:         " << N_ROW << std::endl;
@@ -28,23 +28,12 @@ int main( int argn, char *arg[] )
   
   // allocate some memory
   // ... for matrix and fill it with random values
-  double Aval[NNZ];
-  int AcolInd[NNZ];
-  for ( int i = 0; i < NNZ; i++ ) {
-    Aval[i] = rand() % 5 + 1;
-    AcolInd[i] = rand() % N_COL;
-  }
-  int ArowPt[N_ROW];
-  ArowPt[0] = 0;
-  for ( int i = 1; i < N_ROW; i++ ) {
-    ArowPt[i] = rand() % (NNZ - ArowPt[i-1] - (N_ROW - i)) + ArowPt[i-1] + 1;
-  }
+  double Aval[NNZ] = {1.0, 3.0, 4.0, 2.0, 5.0, 2.0, 1.0};
+  int AcolInd[NNZ] = {1,   3,   2,   0,   4,   2,   3};
+  int ArowPt[N_ROW+1] = {0, 2, 3, 5, 6};
   
   // ... for vector and fill it with random, non-zero, values
-  double Vval[N_COL];
-  for ( int i = 0; i < N_COL; i++ ) {
-    Vval[i] = rand() % 5 + 1;
-  }
+  double Vval[N_COL] = {1.0, 3.0, 4.0, 2.0, 3.0};
   
   // ... for result and make sure, it's zero everywhere
   double Yval[N_ROW];
@@ -62,7 +51,7 @@ int main( int argn, char *arg[] )
   mxv( Aval, AcolInd, ArowPt, Vval, Yval );
   
   // print result if dimenions not too high
-  if ( N_COL < 10 && N_ROW < 10 && NNZ < 15 ) {
+  if ( N_ROW < 10 ) {
     printVec( Yval, N_ROW );
   }
   // print squared norm of solution vector as a measurement for correctness
@@ -79,6 +68,7 @@ void mxv(double * __restrict__ aval,
          double * __restrict__ vval,
          double * __restrict__ yval)
 {
+  std::cout << "Multiplying ..." << std::endl;
   int x, y = 0;
   #pragma omp parallel \
     default(none) \
@@ -89,11 +79,14 @@ void mxv(double * __restrict__ aval,
       schedule(static)
     for ( x = 0; x < N_ROW; x++ ) {
       yval[x] = 0;
+//       printf("Thread %u is doing row=%u (x=%u)\tarowpt[x]=%u\tarowpt[x+1]=%u\n", omp_get_thread_num(), x+1, x, arowpt[x], arowpt[x+1]);
       for ( y = arowpt[x]; y < arowpt[x+1]; y++ ) {
+//         printf("\t[x,y]=[%u,%u]: aval[y]=%f\tacolind[y]=%u\tvval[acolind[y]]=%f\n", x, y, aval[y], acolind[y], vval[acolind[y]]);
         yval[x] += aval[y] * vval[ acolind[y] ];
       }
     }
   } /* end PARALLEL */
+  std::cout << "... done." << std::endl;
 }
 
 /**
@@ -113,7 +106,7 @@ void printSparseMat( double val[], int colInd[], int rowPt[] )
   }
   
   std::cout << std::endl << "\tRowpt: \t";
-  for ( int i = 0; i < N_ROW; i++ ) {
+  for ( int i = 0; i < N_ROW+1; i++ ) {
     std::cout << rowPt[i] << "\t";
   }
   std::cout << std::endl;
